ee031539f6f27981a78b6d8917a879aac16cccd7
Priyal Thummar <thummapr@SYNOVERGETECH.COM>
2024-03-30 16:55:00 +0530

    all changes are done

--- src/helper/multerConfig.ts
+++ src/helper/multerConfig.ts
@@ -16,7 +16,7 @@ const upload = multer({
      storage: storage,
      limits:{fileSize : 1000000},
      fileFilter:(req , file,cb)=>{
-        const fileTypes = /jpeg|jpg|png|gif/
+        const fileTypes = /jpeg|jpg|png|jfif|/
         const mimeTypes = fileTypes.test(file.mimetype)
         const extname = fileTypes.test(path.extname(file.originalname))
 
