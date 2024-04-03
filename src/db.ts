9044b21f39d6cf8d988332ca9bff45b68740521b
priyal38 <priyalthummar38@gmail.com>
2024-04-03 08:33:03 +0530

    final changes

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,11 @@
 
 import mongoose from "mongoose";
+import logger from "./helper/logger";
 
 const connection = async (): Promise<void> => {
     try {
         await mongoose.connect('mongodb://127.0.0.1:27017/fitness-app');
-        console.log('Connected to MongoDB');
+        logger.info('Connected to MongoDB');
         console.log(`Connected to MongoDB database: ${mongoose.connection.name}`);
     } catch (error) {
         console.error(error);
